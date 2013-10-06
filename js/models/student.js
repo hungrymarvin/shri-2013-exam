5d62708ab262049853553c197246335b67852bbe
Maxim Myalin <maxim@myalin.ru>
2013-10-07 00:31:55 +0400

    edit model for Student edit template for Student

--- js/models/student.js
+++ js/models/student.js
@@ -1,22 +1,51 @@
 Shri.Student = DS.Model.extend({
-    name: DS.attr('string'),
-    lastName: DS.attr('string')
+    first_name: DS.attr('string'),
+    last_name: DS.attr('string'),
+    city: DS.attr('string'),
+    about: DS.attr('string'),
+    link_photo: DS.attr('string'),
+    link_facebook: DS.attr('string'),
+    link_vk: DS.attr('string'),
+    link_gihub: DS.attr('string'),
+    link_yaru: DS.attr('string')
 });
 
 Shri.Student.FIXTURES = [
     {
         id: '1',
-        name: 'Name1',
-        lastName: 'LastName1'
+        first_name: 'first_name',
+        last_name: 'last_name',
+        city: 'city',
+        about: 'about',
+        link_photo: 'link_photo',
+        link_facebook: 'link_facebook',
+        link_vk: 'link_vk',
+        link_gihub: 'link_gihub',
+        link_yaru: 'link_yaru'
     },
     {
         id: '2',
-        name: 'Name2',
-        lastName: 'LastName2'
+        first_name: 'first_name',
+        last_name: 'last_name',
+        city: 'city',
+        about: 'about',
+        link_photo: 'link_photo',
+        link_facebook: 'link_facebook',
+        link_vk: 'link_vk',
+        link_gihub: 'link_gihub',
+        link_yaru: 'link_yaru'
     },
     {
         id: '3',
-        name: 'Name3',
-        lastName: 'LastName2'
+        first_name: 'first_name',
+        last_name: 'last_name',
+        city: 'city',
+        about: 'about',
+        link_photo: 'link_photo',
+        link_facebook: 'link_facebook',
+        link_vk: 'link_vk',
+        link_gihub: 'link_gihub',
+        link_yaru: 'link_yaru'
     }
 ];
+
