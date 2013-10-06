b900b4a7a8bee124fc4d9343d2719fb2396c3233
Maxim Myalin <maxim@myalin.ru>
2013-10-07 03:17:34 +0400

    add Student editing

---
+++ js/controllers/student_controller.js
@@ -0,0 +1,13 @@
+Shri.StudentController = Ember.ObjectController.extend({
+    isEditing: false,
+    actions: {
+        edit: function () {
+            this.set('isEditing', true);
+        },
+
+        doneEditing: function () {
+            this.set('isEditing', false);
+            this.get('model').save();
+        }
+    }
+});
