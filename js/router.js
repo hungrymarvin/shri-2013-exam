22ae23d4792686152fc38858a147b8a33292b5f6
Maxim M <maxim@myalin.ru>
2013-10-07 18:11:21 +0400

    add twitter widget

--- js/router.js
+++ js/router.js
@@ -35,7 +35,8 @@ Shri.ApplicationRoute = Ember.Route.extend({
 Shri.AboutRoute = Ember.Route.extend({
     renderTemplate: function(){
         this.render({outlet:'content'});
-
+        this.render('twitter', {
+            outlet: 'sidebar'});
     }
 })
 
